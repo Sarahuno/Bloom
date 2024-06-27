3149ae7c04abe1faad1d8f8ba4801d0b88452f99
Sarahuno <122272550+Sarahuno@users.noreply.github.com>
2024-06-27 20:29:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bloom App - Download & Source Code</title>
+    <style>
+        body {
+            font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #1f2326;
+            color: #e1e1e1;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+        }
+        .container {
+            background-color: #2f3438;
+            border: 2px solid #36404a;
+            padding: 20px;
+            border-radius: 10px;
+            width: 400px;
+            font-size: 20px;
+            text-align: center;
+        }
+        .section {
+            margin-bottom: 20px;
+        }
+        .btn {
+            display: block;
+            margin: 10px auto;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            background-color: #47525a;
+            font-size: 18px;
+            color: #a0a0a0;
+            cursor: pointer;
+            text-decoration: none;
+            width: 88%;
+            text-align: center;
+            transition: background-color 0.3s;
+        }
+        .btn:hover {
+            background-color: #445063;
+        }
+        .cat {
+            position: absolute;
+            bottom: 20px;
+            right: 20px;
+            width: 150px;
+            transition: transform 0.3s ease-in-out;
+        }
+        .cat:hover {
+            transform: scale(1.2);
+            cursor: pointer;
+        }
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <div class="section">
+        <h2>Bloom App - Download & Source Code</h2>
+        <p>Welcome to Bloom App, Bloom App Allows you Too Activate Shikai/Ressurection/Bankai using A selected Hotkey.</p>
+    </div>
+    <div class="section">
+        <a href="https://download848.mediafire.com/y5zfj7ft8plgWR6v4UGNTRPAH8ghxyIYgob1n2iCz62b7wjgThZLF8KlC3P8uN96CehDN5h7IG4n-uC3hFPPZdlGARmaDTYyan24BFUeuF2N29-8sOUnQ1trNCW_hZxvi1HILOYsGoL5FrGjUc8EPwS0KLMZbFNAoiLOUoNx6_pV/48qutfbh9k1o95o/bloom.rar" class="btn">Download App</a>
+        <a href="https://github.com/Sarahuno/Bloom/blob/main/Bloom.py" class="btn">View Source Code</a>
+    </div>
+    <div class="section">
+    <p>There Will Be A console Popup Because if Compiled with no Console it gets Falsely picked up By AntiViruses.</p>
+    <a href="https://coderslegacy.com/pyinstaller-exe-detected-as-virus-solutions/">Why Does Pyinstaller NoConsole Get Flagged By AntiViruses</a>
+    </div>
+</div>
+
+</body>
+</html>
